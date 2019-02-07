3039ad007e038ab78a99c3daaf73b4eb3dbbf376
Antonio <antonio.root@gmail.com>
2019-02-07 17:19:31 +0100

    Shopping cart

--- src/exercise5/Exercise5.js
+++ src/exercise5/Exercise5.js
@@ -1,21 +1,115 @@
-import React from "react";
+import React, { useReducer, useEffect } from "react";
 import { Container } from "../shared/Container";
 import { HomeLink } from "../shared/HomeLink";
 import { Title } from "../shared/Title";
+import { Button } from "../shared/Button";
+
+const increment = "increment";
+function reducer(state, action) {
+  if (action.type === increment) {
+    return state + 1;
+  }
+
+  return state;
+}
 
 export const useTimer = initial => {
-  return initial;
+  const [timer, dispatch] = useReducer(reducer, initial);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      dispatch({ type: increment });
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, []);
+
+  return timer;
+};
+
+const shoppingReducer = (state, action) => {
+  if (action.type === "ADD_TO_CART") {
+    return {
+      ...state,
+      items: {
+        [action.payload.id]: [
+          ...(state.items[action.payload.id] || []),
+          action.payload
+        ]
+      },
+      total: state.total + action.payload.value,
+      previousState: state
+    };
+  }
+
+  if(action.type === 'UNDO'){
+    return state.previousState;
+  }
+
+  return state;
+};
+
+const useShoppingCart = () => {
+  const [cart, dispatch] = useReducer(shoppingReducer, {
+    total: 0,
+    items: {}
+  });
+
+  return {
+    add: item =>
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: item
+      }),
+    remove: item =>
+      dispatch({
+        type: "REMOVE_FROM_CART",
+        payload: item
+      }),
+    undo: () =>
+      dispatch({
+        type: "UNDO"
+      }),
+    cart
+  };
 };
 
 export const Exercise5 = () => {
-  const value = useTimer(1);
+  const { cart, add, undo } = useShoppingCart();
   return (
     <Container>
       <HomeLink />
-      <Title>5. Use Timer</Title>
-      <p>Write a userTimer hook that returns a new value incremented by 1 every second</p>
-      <hr />
-      <p>{value}</p>
+      <Title>Shop</Title>
+      <Button
+        onClick={() => {
+          add({
+            id: 1,
+            name: "baskets",
+            value: 50
+          });
+        }}
+      >
+        50€ - Baskets
+      </Button>
+      <Button
+        onClick={() => {
+          add({
+            id: 2,
+            name: "jeans",
+            value: 30
+          });
+        }}
+      >
+        30€ - Jeans
+      </Button>
+      <Button
+        onClick={() => {
+          undo();
+        }}
+      >
+        Cancel
+      </Button>
+      <p>Total: {cart.total}€</p>
     </Container>
   );
 };
